8ba90e4afdfde3396c3d29b6a3fa2330986f752c
Tiddo <Tiddo@10.76.8.25>
2015-08-18 14:13:04 +0200

    gemaakt door tiddo

--- src/h01/HalloRocDev.java
+++
@@ -1,9 +0,0 @@
-package h01;
-
-public class HalloRocDev {
-
-	public static void main(String[] args) {
-		System.out.println("Hallo roc-dev!");
-	}
-
-}
---
+++ src/h01/HelloRocDev.java
@@ -0,0 +1,9 @@
+package h01;
+
+public class HelloRocDev {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo alles goed?");
+	}
+
+}
